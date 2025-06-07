d67545afed1df45fc528c3e5eec954f64828f000
bugRanger <departmentofjusticeevil@gmail.com>
2025-06-07 11:11:15 +0700

    Add traits for request and response

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use std::fmt::Debug;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+
+/// The Request Method.
+///
+/// This type also contains constants for a number of common HTTP methods such as GET, POST, etc.
+#[derive(Copy, Clone)]
+pub enum RequestMethod {
+    Get,
+    Post,
+    Delete,
+    Put,
+}
+
+impl RequestMethod {
+    pub fn as_str(&self) -> &str {
+        match self {
+            RequestMethod::Get => "GET",
+            RequestMethod::Post => "POST",
+            RequestMethod::Delete => "DELETE",
+            RequestMethod::Put => "PUT",
+        }
+    }
+}
+
+pub trait Request: Serialize {
+    const METHOD: RequestMethod;
+    const PATH: &'static str;
+    const IN_URI: bool = false;
+    type Response: Response;
+}
+
+pub trait Response: DeserializeOwned + Debug {
+    type Data: DeserializeOwned + Debug;
+
+    fn extract(self) -> anyhow::Result<Self::Data>;
+}
